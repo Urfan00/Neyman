f69ff613595e1fedbfbb7ec91dbe2b985d451237
Urfan00 <urfanagazade75@gmail.com>
2023-08-23 19:06:51 +0400

    Finish

--- Neyman/settings.py
+++ Neyman/settings.py
@@ -85,13 +85,23 @@ WSGI_APPLICATION = 'Neyman.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'neymantech',
+        'USER': 'adminney',
+        'PASSWORD': 'neymantech@0122',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
